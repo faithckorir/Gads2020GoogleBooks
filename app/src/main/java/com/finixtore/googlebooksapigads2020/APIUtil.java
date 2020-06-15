d5205572546366b2f3774fdb71496f1ba8a803f4
Faith Korir <faithckorir@gmail.com>
2020-06-15 23:38:05 +0300

    connection disconnected

--- app/src/main/java/com/finixtore/googlebooksapigads2020/APIUtil.java
+++ app/src/main/java/com/finixtore/googlebooksapigads2020/APIUtil.java
@@ -1,6 +1,14 @@
 package com.finixtore.googlebooksapigads2020;
 
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
+import java.nio.file.OpenOption;
+import java.util.Scanner;
 
 public class APIUtil {
     private APIUtil() {
@@ -21,5 +29,31 @@ public class APIUtil {
         }
 
         return url;
+    }
+    public static  String getJsonData(URL url) throws IOException {
+
+        //first establish a connection using http connection
+        HttpURLConnection connection= (HttpURLConnection) url.openConnection();
+
+        try {
+            InputStream stream=connection.getInputStream();
+            Scanner scanner=new Scanner(stream);
+            scanner.useDelimiter("\\A");
+            boolean hasdata=scanner.hasNext();
+
+
+
+        if (hasdata){
+            return scanner.next();
+
+        }else {
+            return null;
+        }}catch (Exception e){
+            Log.d("Error Message",e.toString());
+            return null;
+        }
+        finally {
+            connection.disconnect();
+        }
     }
 }
